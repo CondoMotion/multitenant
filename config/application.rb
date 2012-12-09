c49ddf77fe9c28a1acaa7d5ce15710970a69d418
David Van Der Beek <earlynovrock@gmail.com>
2012-12-09 17:01:17 -0600

    precompile home css

--- config/application.rb
+++ config/application.rb
@@ -57,6 +57,7 @@ module Forum
     config.assets.enabled = true
 
     config.assets.initialize_on_precompile = false
+    config.assets.precompile += ['home.css']
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
