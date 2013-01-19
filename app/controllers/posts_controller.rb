e4b5236875cf3b3c26834745fe1b14c8443345b3
obowen <oebowen@gmail.com>
2013-01-19 09:34:10 -0500

    Fixed error caused by photo and document partials

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,32 +6,32 @@ class PostsController < ApplicationController
   end
 
   def create
-	  @post = current_user.posts.new(params[:post])
-	  @post.page = Page.find(params[:page_id])
-	  @site = @post.page.site 
-	  @page = @post.page
-
-	  if @post.save
-	  	PostMailer.new_post(@post, @post.attachment.url.nil? ? "" : @post.attachment.url).deliver
-	    flash[:notice] = 'Post was successfully created.'
-	    #redirect_to( page_url(@post.page, subdomain: @site.subdomain) )
-            redirect_to(:back)
-	  else
-	    flash[:error] = 'Post could not be created.'
-	    render template: "pages/show"
-	  end
-	end
-
-	def edit 
-		@post = Post.find(params[:id])
-		@page = @post.page
-		# @site = @post.page.site
-	end
-
-	def update
-		@post = Post.find(params[:id])
-
-		respond_to do |format|
+    @post = current_user.posts.new(params[:post])
+    @post.page = Page.find(params[:page_id])
+    @site = @post.page.site 
+    @page = @post.page
+    
+    if @post.save
+      PostMailer.new_post(@post, @post.attachment.url.nil? ? "" : @post.attachment.url).deliver
+      flash[:notice] = 'Post was successfully created.'
+      #redirect_to( page_url(@post.page, subdomain: @site.subdomain) )
+      redirect_to(:back)
+    else
+      flash[:error] = 'Post could not be created.'
+      render template: "pages/show"
+    end
+  end
+  
+  def edit 
+    @post = Post.find(params[:id])
+    @page = @post.page
+    # @site = @post.page.site
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    
+    respond_to do |format|
       if @post.update_attributes(params[:post])
         format.html { redirect_to @post, notice: 'Post was successfully updated.' }
         format.json { head :no_content }
@@ -40,16 +40,16 @@ class PostsController < ApplicationController
         format.json { render json: @post.errors, status: :unprocessable_entity }
       end
     end
-	end
-
-	def destroy
+  end
+  
+  def destroy
     @post = Post.find(params[:id])
     @post.destroy
-
+    
     respond_to do |format|
       format.html { redirect_to page_url(@post.page, subdomain: @post.page.site.subdomain), notice: "Post deleted." }
       format.json { head :no_content }
     end
   end
-
+  
 end
